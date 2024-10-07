1f0d9e6024e46ff4bc7004851c981467aaf473ef
Viktor <test@gmail.com>
2024-10-07 16:27:08 +0200

    Loading of elements from the server has been implemented

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,5 +1,5 @@
 //import checkNumInputs from "./checkNumInputs";
-
+import { postData } from "../services/requests";
 const forms = () => {
     const form = document.querySelectorAll('form'),
           inputs = document.querySelectorAll('input'),
@@ -20,15 +20,6 @@ const forms = () => {
         designer: 'assets/server.php',
         question: 'assets/question.php'
     }
-
-    const postData = async (url, data) => {
-
-        let res = await fetch(url, {
-            method: 'POST',
-            body: data
-        });
-        return await res.text();
-    };
 
     const clearInputs = () => {
         inputs.forEach(item => {
